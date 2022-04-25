dd3a1c61901e3390a8aa72f25429d34e5de3dac9
BrieMac <brienna.macnish@envato.com>
2022-04-26 08:23:14 +1000

    Fix message input styling

--- screens/MessageDetailsScreen.tsx
+++ screens/MessageDetailsScreen.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import { FontAwesome } from '@expo/vector-icons'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import Bubble from '../components/Bubble'
@@ -13,7 +14,7 @@ const CONVERSATION_DATA = [
     reply: true,
   },
   {
-    text: 'Plannign out some designs for this messaging app! What do you think?',
+    text: 'Planning out some designs for this messaging app! What do you think?',
   },
   {
     text: 'Looking good, looking forward to seeing the final thing',
@@ -23,27 +24,40 @@ const CONVERSATION_DATA = [
     text: 'Good luck!',
     reply: true,
   },
+  {
+    text: 'Thanks',
+  },
 ]
 
 export default function MessageDetailsScreen() {
   const [conversationIndex, setConversationIndex] = useState(0)
 
-  const advanceConversation = () => {
-    setConversationIndex(conversationIndex + 1)
-    console.log(conversationIndex)
-  }
-
   return (
     <View style={styles.container}>
       {CONVERSATION_DATA.map((bubbleData, index) => {
         if (index <= conversationIndex) {
-          return <Bubble text={bubbleData.text} reply={bubbleData.reply} />
+          return (
+            <Bubble
+              text={bubbleData.text}
+              reply={bubbleData.reply}
+              key={index}
+            />
+          )
         }
       })}
 
-      <TouchableOpacity style={styles.advance} onPress={advanceConversation}>
-        <Text>Advance</Text>
-      </TouchableOpacity>
+      <View style={styles.inputContainer}>
+        <View style={styles.input}>
+          <Text>Message</Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            setConversationIndex(conversationIndex + 1)
+          }}
+        >
+          <FontAwesome name='send' size={24} color='black' />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
@@ -54,11 +68,17 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     justifyContent: 'flex-end',
   },
-  advance: {
-    borderRadius: 50,
-    marginVertical: 20,
-    backgroundColor: 'lightblue',
-    padding: 20,
-    alignItems: 'center',
+  inputContainer: {
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 20,
+    flexDirection: 'row',
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    marginBottom: 24,
+  },
+  input: {
+    flex: 2,
+    justifyContent: 'center',
   },
 })
