691203cb88c530968bc308509388f8fc1d2959e0
bmacnish <brienna.macnish@gmail.com>
2022-04-26 09:03:15 +1000

    [WIP] Working on conversation data formatt

--- screens/MessageDetailsScreen.tsx
+++ screens/MessageDetailsScreen.tsx
@@ -1,44 +1,72 @@
 import { FontAwesome } from '@expo/vector-icons'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import Bubble from '../components/Bubble'
 import { View, Text } from '../components/Themed'
 
-const CONVERSATION_DATA = [
-  {
+const CONVERSATION_DATA = {
+  oneFriend: {
     message: 'Hey, what are you up to?',
     options: {
       replyOne: {
         id: '1',
         text: 'Not much.',
+        next: 'twoFriend',
+        timeout: 500,
       },
       replyTwo: {
         id: '2',
         text: 'Doing some chores.',
+        next: 'threeFriend',
+        timeout: 700,
       },
     },
-    timeout: 500,
   },
-]
+}
 
 export default function MessageDetailsScreen() {
-  const [conversationIndex, setConversationIndex] = useState(0)
+  const currentConversation = [CONVERSATION_DATA.oneFriend]
+  const [currentEvent, setCurrentEvent] = useState(CONVERSATION_DATA.oneFriend)
+
+  const handleResponse = useCallback((reply) => {
+    console.log({ reply })
+  }, [])
 
   return (
     <View style={styles.container}>
-      {CONVERSATION_DATA.map((bubbleData) => (
-        <Bubble text={bubbleData.message} />
-      ))}
+      {currentConversation.map((bubbleData, index) => {
+        console.log({ currentEvent, bubbleData })
+        if (bubbleData !== currentEvent) setCurrentEvent(bubbleData)
+
+        return <Bubble text={bubbleData.message} key={index} />
+      })}
 
       <View style={styles.inputContainer}>
         <View style={styles.input}>
-          <Text>Message</Text>
+          <Text>{currentEvent.options.replyOne.text}</Text>
         </View>
         <TouchableOpacity
-          onPress={() => {
-            setConversationIndex(conversationIndex + 1)
-          }}
+          onPress={() => handleResponse(currentEvent.options.replyOne)}
+        >
+          <FontAwesome name="send" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text>OR</Text>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <View style={styles.input}>
+          <Text>{currentEvent.options.replyTwo.text}</Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => handleResponse(currentEvent.options.replyTwo)}
         >
           <FontAwesome name="send" size={24} color="black" />
         </TouchableOpacity>
@@ -60,7 +88,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     paddingVertical: 8,
     paddingHorizontal: 12,
-    marginBottom: 24,
+    marginVertical: 12,
   },
   input: {
     flex: 2,
